2d2a93255460ea18da546da037e394e0a0e2e78f
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2024-02-04 15:24:12 -0500

    presence_in wow

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,7 +1,7 @@
 class InvoicesController < ApplicationController
   def index
     sort_column = params[:sort] || "created_at"
-    sort_direction = %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    sort_direction = params[:direction].presence_in(%w[asc desc]) || "desc"
 
     @invoices = Invoice.order("#{sort_column} #{sort_direction}").page(params[:page]).per(10)
   end
